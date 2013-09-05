6323063cb56b497ef421d3bb3c4b711095849e3f
Damien de Saint Jorre <damien.desaintjorre@epfl.ch>
2013-09-05 14:06:56 +0200

    (Hevc): Updating GenerateInterPredFiles.sh.

--- HEVC/utils/GenerateInterPredFiles.sh
+++ HEVC/utils/GenerateInterPredFiles.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 #Generating InterPredPipeline1 thanks to the InterPrediction actor
-cp ../../RVC/src/devel/org/sc29/wg11/mpegh/part2/inter/InterPrediction.cal ../src/interPrediction/InterPredPipeline1.cal
-patch ../src/interPrediction/InterPredPipeline1.cal < InterPredPipeline1.patch
+sed -e 's/package devel.org.sc29.wg11.mpegh.part2.main.inter/package interPrediction/' \
+    -e 's/actor InterPrediction/actor InterPredPipeline1/'\
+    -e 's/uint(size=8) PredSample/int (size=16) PredSample/' < ../../RVC/src/devel/org/sc29/wg11/mpegh/part2/main/inter/InterPrediction.cal\
+	> ../src/interPrediction/InterPredPipeline1.cal
 
 #Generating InterPredPipeline2 thanks to the InterPrediction actor
-cp ../../RVC/src/devel/org/sc29/wg11/mpegh/part2/inter/InterPrediction.cal ../src/interPrediction/InterPredPipeline2.cal
-patch ../src/interPrediction/InterPredPipeline2.cal < InterPredPipeline2.patch
+sed -e 's/package devel.org.sc29.wg11.mpegh.part2.main.inter/package interPrediction/' \
+    -e 's/actor InterPrediction/actor InterPredPipeline2/'\
+    -e 's/uint(size=8)  Sample/int (size=16) Sample/' < ../../RVC/src/devel/org/sc29/wg11/mpegh/part2/main/inter/InterPrediction.cal\
+	> ../src/interPrediction/InterPredPipeline2.cal
 
